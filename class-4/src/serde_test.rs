8ad452aec22c34023ec6a43e0410d4265b33ce57
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-11-12 17:59:04 -0300

    patch(class-4): add new `deserialize` examples

--- class-4/src/serde_test.rs
+++ class-4/src/serde_test.rs
@@ -1,20 +1,22 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Pessoa {
     altura: u8,
     pub nome: String,
 }
 
-pub fn resposta_do_endpoint() {
+pub fn resposta_do_endpoint() -> eyre::Result<()> {
     let pessoa = Pessoa {
         altura: 173,
         nome: "Gustavo".to_string(),
     };
 
-    let pessoa_json = serde_json::to_string(&pessoa);
+    let pessoa_json = serde_json::to_string(&pessoa)?;
 
-    if let Ok(meu_json) = pessoa_json {
-        println!("{}", meu_json);
-    }
+    println!("{}", pessoa_json);
+
+    let pessoa_nova = serde_json::from_str::<Pessoa>(&pessoa_json)?;
+
+    Ok(())
 }
